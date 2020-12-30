d30ca6e86db185033690d38c8896fcabc839a733
ganjiro <girolamomacaluso@gmail.com>
2020-12-30 21:57:45 +0100

    Added GetVariable working Added GetOrder Not Tested

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import random
 import itertools
+from math import trunc
 
 
 def LoadRandomSolvedSudoku():
@@ -43,7 +44,6 @@ def ArcConsistency(puzzle):
 
 def Revise(index,puzzle):
     revised = False
-    print(index)
     for x in puzzle.domains[index[0][0]][index[0][1]]:
         if x == puzzle.variables[index[1][0]][index[1][1]]:
             puzzle.domains[index[0][0]][index[0][1]].remove(x)
@@ -52,21 +52,22 @@ def Revise(index,puzzle):
 
 
 class Sudoku:
-    def __init__(self):
-        self.variables = None
-        self.costraints = self.CreateCostraints()
-        print(self.costraints)
-        self.domains = [[[1,2,3,4,5,6,7,8,9] for _ in range(0, 9)] for _ in range(0, 9)]
-
-    def LoadSudoku(self, puzzle):
+    def __init__(self,puzzle):
         self.variables = puzzle
+        self.costraints = self.createCostraints()
+        self.domains = self.createDomains()
+        print(self.domains[7][0])
         ArcConsistency(self)
+        print(self.domains[7][0])
+
 
     def isComplete(self):
-        result = True
+        for i,j in itertools.product(range(0, 9), range(0, 9)):
+            if self.variables[i][j]==0:
+                return False
+        return True
 
-
-    def CreateCostraints(self):
+    def createCostraints(self):
         costraints = []
         costraints += [[[i, j], [i, k]] for i, j, k in itertools.product(range(0, 9), range(0, 9), range(0, 9))]
         costraints += [[[i, j], [k, j]] for i, j, k in itertools.product(range(0, 9), range(0, 9), range(0, 9))]
@@ -79,7 +80,47 @@ class Sudoku:
             if [[i, j], [i, j]] in costraints: costraints.remove([[i, j], [i, j]])
         return costraints
 
+    def createDomains(self):
+        domains = [[[1,2,3,4,5,6,7,8,9] for _ in range(0, 9)] for _ in range(0, 9)]
+        for i, j in itertools.product(range(0, 9), range(0, 9)):
+            if self.variables[i][j] != 0:
+                domains[i][j] = [self.variables[i][j]]
+        return domains
+
+    def getVariable(self):
+        domainDimension = [len(self.domains[i][j]) for i,j in itertools.product(range(0, 9), range(0, 9)) if self.variables[i][j] == 0]
+        vars = [i for i, x in enumerate(domainDimension) if x == min(domainDimension)]
+        return [trunc(vars[0]/9), vars[0] % 9]
+
+    def getOrder(self,var):
+        rating = [0 for _ in range(0, 10)]
+        for i in range (0,9):
+            for k in self.domains[var[0]][i]:
+                rating[k] += 1
+            for k in self.domains[i][var[1]]:
+                rating[k] += 1
+        x = var[0] - var[0] % 3
+        y = var[1] - var[1] % 3
+        for i,j  in itertools.product(range(x, x + 3), range(y, y + 3)):
+            for k in self.domains[i][j]:
+                rating[k] += 1
+        finalOrder = []
+        for i in rating:
+            finalOrder.append(rating.index(max(rating)))
+
+
+
+
+def backTrackingSudoku(sudoku):
+    if sudoku.isComplete(): return sudoku
+    var = sudoku.getVariable()
+
+
+
+
+
+
 
 a=[[0,0,3,0,2,0,6,0,0],[9,0,0,3,0,5,0,0,1],[0,0,1,8,0,6,4,0,0],[0,0,8,1,0,2,9,0,0],[7,0,0,0,0,0,0,0,8],[0,0,6,7,0,8,2,0,0],[0,0,2,6,0,9,5,0,0],[8,0,0,2,0,3,0,0,9],[0,0,5,0,1,0,3,0,0,]]
-s = Sudoku()
-s.LoadSudoku(a)
+s = Sudoku(a)
+backTrackingSudoku(s)
